715f391a06b75d0a43e9615a209eaae4ced09604
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-12-04 17:26:13 +0700

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -4,7 +4,7 @@ if [ $result -eq 0 ]
 then
 echo "OK"
 #rsync -e ssh Docker/source/*.php  --exclude='Docker/source/autoload.php' root@192.168.141.203:/usr/share/nginx/html
-mv Docker/*.php  /usr/share/nginx/html/
+mv Docker/*.php  /
 else
 echo "Failed"
 fi
