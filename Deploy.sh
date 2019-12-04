bf81c8e037e46553b09717edab272c4f4f198e42
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-12-04 17:26:03 +0700

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-result=$(awk '{if ($1 == 0 || $1 == 1) print $1}' Demo_jenkins/result.txt)
+result=$(awk '{if ($1 == 0 || $1 == 1) print $1}' Docker/result.txt)
 if [ $result -eq 0 ]
 then
 echo "OK"
-#rsync -e ssh Demo_jenkins/source/*.php  --exclude='Demo_jenkins/source/autoload.php' root@192.168.141.203:/usr/share/nginx/html
-mv Demo_jenkins/*.php  /usr/share/nginx/html/
+#rsync -e ssh Docker/source/*.php  --exclude='Docker/source/autoload.php' root@192.168.141.203:/usr/share/nginx/html
+mv Docker/*.php  /usr/share/nginx/html/
 else
 echo "Failed"
 fi
