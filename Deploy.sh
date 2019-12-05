7f2257ce1efb1b7a5d2fedda9c441794a762e0ed
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-12-05 10:21:34 +0700

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -3,7 +3,7 @@ result=$(awk '{if ($1 == 0 || $1 == 1) print $1}' ./result.txt)
 if [ $result -eq 0 ]
 then
 echo "mv ./src/*.php  /usr/share/nginx/html/"
-pwd
+mv ./src/*.php  /usr/share/nginx/html/
 #rsync -e ssh Docker/source/*.php  --exclude='Docker/source/autoload.php' root@192.168.141.203:/usr/share/nginx/html
 else
 echo "Failed"
