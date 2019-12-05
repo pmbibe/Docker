5db6571ae7b0f54e20e184928c871bf7c1204fb3
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-12-05 10:08:29 +0700

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-result=$(awk '{if ($1 == 0 || $1 == 1) print $1}' Docker/result.txt)
+result=$(awk '{if ($1 == 0 || $1 == 1) print $1}' Jenkins_Dockerfile/result.txt)
 if [ $result -eq 0 ]
 then
 echo "OK"
+hostname
 #rsync -e ssh Docker/source/*.php  --exclude='Docker/source/autoload.php' root@192.168.141.203:/usr/share/nginx/html
-mv Docker/*.php  /
+#mv Jenkins_Dockerfile/*.php  /
 else
 echo "Failed"
 fi
