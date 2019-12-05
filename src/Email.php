4e26227b6228240ced43a6a5be2b5633e1a70c7f
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-12-05 13:40:58 +0700

    Update Email.php

--- src/Email.php
+++ src/Email.php
@@ -1,21 +1,36 @@
 <?php
 declare(strict_types=1);
+/**
+* @covers Calculator::A
+*/
 final class Email
 {
     private $email;
+    /**
+    * @covers Calculator::Test
+    */
     private function __construct(string $email)
     {
         $this->ensureIsValidEmail($email);
         $this->email = $email;
     }
+     /**
+     * @covers Calculator::B
+     */
     public static function fromString(string $email): self
     {
         return new self($email);
     }
+     /**
+     * @covers Calculator::C
+     */
     public function __toString(): string
     {
         return $this->email;
     }
+    /**
+     * @covers Calculator::D
+     */
     private function ensureIsValidEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
